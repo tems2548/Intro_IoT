50da27783883a508603e9907c085cb6955e6110e
TemZ <110783208+tems2548@users.noreply.github.com>
2025-07-09 09:36:25 +0700

    Update Led_12.cpp to real PWM signal

--- Week 1/Led_12.cpp
+++ Week 1/Led_12.cpp
@@ -8,15 +8,17 @@ void setup() {
 }
 
 void loop() {
+  // led 1 period is 1 second (1 Hz)
   // led 2 period is 0.5 second (2 Hz)
+
   digitalWrite(led_2,1);
+  digitalwrite(led_1,1);
   delay(250);
   digitalWrite(led_2,0);
   delay(250);
-
-  // led 1 period is 1 second (1 Hz)
-  digitalWrite(led_1,1);
-  delay(250);
+  digitalWrite(led_2,1);
   digitalWrite(led_1,0);
+  delay(250);
+  digitalWrite(led_2,0);
   delay(250);
 }
